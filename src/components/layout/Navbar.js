90d7b88acc7565d1d895c7354cd067f86843ece3
lorijr <jrsilva.lorivas@gmail.com>
2026-06-06 11:36:07 -0300

    fix: navbar module css

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom"
 
 import Container from "./Container"
 import styles from './Navbar.module.css'
-import logo from '../../img/logo.png'
+import logo from '../../img/apple-touch-icon.png'
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
             <Container>
                 <Link to="/">
-                    <img src={logo} alt="Viratech" />
+                    <img src={logo} alt="Viratech" width={70}/>
                 </Link>
-                <ul className src={styles.list}>
+                <ul className={styles.list}>
                     <li className={styles.item}><Link to="/">Home</Link></li>
                     <li className={styles.item}><Link to="/signin">Signin</Link></li>
                 </ul>
